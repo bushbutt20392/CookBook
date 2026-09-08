03a282084b382ba25bd03e6e153b69da6267ecbe
bushbutt20392 <bushbutt4@gmail.com>
2026-09-08 00:00:54 -0700

    My CookBook

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,10 @@
+function RecipeCard(){
+    return(
+        <div>
+            <h3>Chicken Biryani</h3>
+            <p>Delicious Pakistani rice dish.</p>
+        </div>
+    )
+}
+
+export default RecipeCard
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,91 @@
 function Home() {
     return (
-        <h1> CookBook Home</h1>
+        <div>
+        <section>
+              <h1> Welcome to CookBook</h1>
+              <p>Discover delicious and easy recipes for everyone.</p>
+              <button> Explore Recipes</button>
+        </section>
+
+          <section>
+              <h2> Popular Recipes</h2>
+              <p>Explore some of our most popular Recipes.</p>
+              <div>
+                <h3>Chicken Biryani</h3>
+                <p>Delicious Pakistani rice dish.</p>
+              </div>
+               <div>
+                <h3>Pasta</h3>
+                 <p>Creamy and tasty pasta.</p>
+              </div>
+               <div>
+                <h3>Chocolate Cake</h3>
+                <p>Soft and delicious chocolate cake.</p>
+              </div>
+        </section>
+
+           <section>
+              <h2> Categories</h2>
+              <p>Browse recipes by category.</p>
+
+              <div>
+                <h3>Chicken</h3>
+                <p>Delicious chicken recipes.</p>
+              </div>
+               <div>
+                <h3>Pasta</h3>
+                <p>Delicious pasta recipes.</p>
+              </div>
+               <div>
+                <h3>Salad</h3>
+                <p>Fresh and healthy salad recipes.</p>
+              </div>
+               <div>
+                <h3>Dessert</h3>
+                <p>Sweet and delicious dessert recipes.</p>
+              </div>
+        </section>
+
+          <section>
+              <h2> Featured Recipe</h2>
+              <p>Try our special recipe of the day.</p>
+               <div>
+                <h3>Chicken Biryani</h3>
+                <p>A delicious and flavorful recipe you must try.😋🍴</p>
+              </div>
+        </section>
+
+          <section>
+              <h2> Why CookBook</h2>
+              <p>Discover  why CookBook is a great place for food lovers.</p>
+                <div>
+                <h3>Easy Recipes</h3>
+                <p>Simple recipe that are easy to follow.</p>
+              </div>
+                <div>
+                <h3>Cokking Tips</h3>
+                <p>Useful tips to make your cooking better.</p>
+              </div>
+                <div>
+                <h3>New Ideas</h3>
+                <p>Find new and delicious meal ideas.</p>
+              </div>
+        </section>
+
+          <section>
+              <h2> Newsletters</h2>
+              <p>Subscribe to get new recipes and cooking ideas.</p>
+              <div>
+                <input type="email" placeholder="Enter your email" />
+
+                <button>
+                    Subscribe
+                </button>
+              </div>
+        </section>
+        </div>
     )
+        
+    
 }
 export default Home
